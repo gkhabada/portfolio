ff7368268f756501b7e3480d64fd3c7c2a04243a
Gadzhidadaev Gadzhi <gadzhi7.7.7@mail.ru>
2017-12-27 16:05:58 +0300

    added a little script 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
 
 	var w = $('.work').css('width');
@@ -12,10 +10,34 @@ $(window).resize(function(){
 	$('.work').css('height', h + 'px');
 });
 
+
+
 $('.menu__link').click(function(event) {
 	event.preventDefault();
 	var link =  $(this).attr('href');
 	$('html, body').animate({ scrollTop: $(link).offset().top }, 500);
 });
+
+
+
+$('.contacts_animation').focusin(function() {
+	var elemHeight = $(this).css('height');
+	var elemTop = parseInt(elemHeight) + 30;
+	var elem = $(this).next().css({'top': -elemTop + 'px', 'color': '#23c869'});
+	$(this).css({'border-color': '#23c869'});
+	console.log(elem);
+});
+$('.contacts_animation').focusout(function() {
+	var elemHeight = $(this).css('height');
+	var elemTop = parseInt(elemHeight) - 5;
+	$(this).next().css({'top': -elemTop + 'px',  'color': '#404040' });
+	$(this).css({'border-color': '#404040'});
+});
+
+
+
+
+
+
 
 });
