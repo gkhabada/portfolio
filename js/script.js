fbbd10904ab8d1db61e6d09c332521f75c4b6d4d
Gadzhidadaev Gadzhi <gadzhi7.7.7@mail.ru>
2017-12-30 14:37:19 +0300

    completed desktop version of site  and animation on the page

--- js/script.js
+++ js/script.js
@@ -17,28 +17,72 @@ $(document).ready(function() {
 		var link =  $(this).attr('href');
 		$('html, body').animate({ scrollTop: $(link).offset().top }, 800);
 	});
+	
+	$('.scroll-top').click(function(event) {
+		$('html, body').animate({ scrollTop: 0}, 800);
+	});
+	
+	$(document).scroll(	function () {
+		var top = $("html, body").scrollTop();
+		if (top > 200) {
+			$('.scroll-top').fadeIn(500);
+		} else {
+			$('.scroll-top').fadeOut(500);
+		}
+	});
 
 
+
+	
+	
+	
 
 	$('.contacts_animation').focusin(function() {
 		var elemHeight = $(this).css('height');
 		var elemTop = parseInt(elemHeight) + 30;
 		var elem = $(this).next().css({'top': -elemTop + 'px', 'color': '#23c869'});
 		$(this).css({'border-color': '#23c869'});
-		console.log(elem);
 	});
 	$('.contacts_animation').focusout(function() {
 		var elemHeight = $(this).css('height');
 		var elemTop = parseInt(elemHeight) - 5;
-		$(this).next().css({'top': -elemTop + 'px',  'color': '#404040' });
+		v = $(this).val();
+		if (v) {
+			$(this).next().css({'color': '#404040' });
+			$(this).css({'color': '#27b262', 'font-family': 'mullerbold'});
+		} else {
+			$(this).next().css({'top': -elemTop + 'px',  'color': '#404040' });
+		}
 		$(this).css({'border-color': '#404040'});
 	});
 
-	$(document).ready(function() {
-		$('.section').addClass("hidden").viewportChecker({
-			classToAdd: 'visible animated fadeIn', 
-			offset: 100    
-		});   
-	});            
+	
+	
+	
+	
+	$('.section').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated fadeIn', 
+		offset: 100    
+	});   
+	
+	
+	
+	
+	$('.portfolio__show-more').click(function() {
+		$('.work_part2').css({display: 'block'});
+		$(this).animate({opacity: 0}, 50);
+		$('.work_part2').animate({opacity: 1}, 500);
+	});
+	
 
+	/*
+	
+	setTimeout (function(){
+		$('body, .header, .portfolio, .about-me, .contacts, h1, h2, h3, p, span, .menu__home').removeClass('white').addClass('black');
+		$('.header__link, .portfolio__show-more ').removeClass('.white_button').addClass('.black_button');
+	}, 3000);
+	
+	*/
+	
+	
 });
