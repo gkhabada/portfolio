4bdebd28ec02e43e7b1556dae12d2a8ffd99ff95
Gadzhi <gadzhidadaev95@gmail.com>
2019-02-17 11:51:56 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
 	var w = $('.work').css('width');
-	console.log(w);
 	var h = parseInt(w) / 100 *56.25;
 	$('.work').css('height', h + 'px');
-	$(window).resize(function(){	
+	$(window).resize(function(){
 		var w = $('.work').css('width');
 		var h = parseInt(w) / 100 *56.25;
 		$('.work').css('height', h + 'px');
@@ -17,11 +16,11 @@ $(document).ready(function() {
 		var link =  $(this).attr('href');
 		$('html, body').animate({ scrollTop: $(link).offset().top }, 800);
 	});
-	
+
 	$('.scroll-top').click(function(event) {
 		$('html, body').animate({ scrollTop: 0}, 800);
 	});
-	
+
 	$(document).scroll(	function () {
 		var top = $("html, body").scrollTop();
 		if (top > 200) {
@@ -33,9 +32,9 @@ $(document).ready(function() {
 
 
 
-	
-	
-	
+
+
+
 
 	$('.contacts_animation').focusin(function() {
 		var elemHeight = $(this).css('height');
@@ -56,33 +55,59 @@ $(document).ready(function() {
 		$(this).css({'border-color': '#404040'});
 	});
 
-	
-	
-	
-	
+
+
+
+
 	$('.section').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated fadeIn', 
-		offset: 100    
-	});   
-	
-	
-	
-	
-	$('.portfolio__show-more').click(function() {
-		$('.work_part2').css({display: 'block'});
-		$(this).animate({opacity: 0}, 50);
-		$('.work_part2').animate({opacity: 1}, 500);
+		classToAdd: 'visible animated fadeIn',
+		offset: 100
 	});
-	
+
+
+let showWork = 0;
+function showWorkFunc(){
+	for (var i = 0; i < 8; i++) {
+		$(elem[showWork]).fadeIn(1000);
+		showWork++;
+	}
+	if($(elem).length <= showWork) {
+		$('.portfolio__show-more').hide(200);
+	}
+};
+
+
+let elem = $(".works").children();
+elem.hide();
+$('.portfolio__show-more').on("click", showWorkFunc);
+showWorkFunc();
 
 	/*
-	
 	setTimeout (function(){
 		$('body, .header, .portfolio, .about-me, .contacts, h1, h2, h3, p, span, .menu__home').removeClass('white').addClass('black');
 		$('.header__link, .portfolio__show-more ').removeClass('.white_button').addClass('.black_button');
 	}, 3000);
-	
 	*/
-	
-	
+
+
+	$(function() {
+	    $("#contacts__submit").click(function() {
+					let formData = new FormData();
+					formData.append("name", $("#contacts__name").val());
+					formData.append("email", $("#contacts__email").val());
+					formData.append("telegram", $("#contacts__telegram").val());
+					formData.append("message", $("#contacts__message").val());
+
+					$.ajax({
+						type: "POST",
+						url: "../formdata.php",
+						data: formData,
+						success: function(){
+							$("#contacts__name, #contacts__email, #contacts__telegram, #contacts__message").val('');
+							console.log('success');
+						}
+					});
+	        return false;
+	    });
+	});
 });
