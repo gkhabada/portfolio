287cf8c17b49b5fcbffb4f09c329594092e0641e
gadzhi <gadzhi7.7.7@mail.ru>
2019-04-06 23:54:37 +0300

    fixed bugs and optimized processes and update soc icons

--- js/script.js
+++ js/script.js
@@ -1,121 +1,202 @@
 $(document).ready(function() {
 
-	var w = $('.work').css('width');
-	var h = parseInt(w) / 100 *56.25;
-	$('.work').css('height', h + 'px');
-	$(window).resize(function(){
-		var w = $('.work').css('width');
-		var h = parseInt(w) / 100 *56.25;
-		$('.work').css('height', h + 'px');
-	});
+  // menu scroll
+
+  $('.menu__link').click(function(event) {
+    event.preventDefault();
+    var link = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(link).offset().top
+    }, 800);
+  });
+
+  // button scrollTop
+
+  $(document).scroll(function() {
+    var top = $("html, body").scrollTop();
+    if (top > 200) {
+      $('.scroll-top').fadeIn(500);
+    } else {
+      $('.scroll-top').fadeOut(500);
+    }
+  });
+
+  $('.scroll-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+  });
+
+  // animation lable on contacts block
+
+  let elemHeight, elemTop, elem;
+
+  $('.contacts_animation').focusin(function() {
+    elemHeight = $(this).css('height');
+    elemTop = parseInt(elemHeight) + 30;
+    elem = $(this).next().css({
+      'top': -elemTop + 'px',
+      'color': '#23c869'
+    });
+    $(this).css({
+      'border-color': '#23c869'
+    });
+  });
+
+  $('.contacts_animation').focusout(function() {
+    elemHeight = $(this).css('height');
+    elemTop = parseInt(elemHeight) - 5;
+    v = $(this).val();
+    if (v) {
+      $(this).next().css({
+        'color': '#404040'
+      });
+      $(this).css({
+        'color': '#27b262',
+        'font-family': 'mullerbold'
+      });
+    } else {
+      $(this).next().css({
+        'top': -elemTop + 'px',
+        'color': '#404040'
+      });
+    }
+    $(this).css({
+      'border-color': '#404040'
+    });
+  });
+
+  // animate show block
+
+  // $('.section').addClass("hidden").viewportChecker({
+  //   classToAdd: 'visible animated fadeIn',
+  //   offset: 100
+  // });
 
 
+  let showWork = 0;
 
-	$('.menu__link').click(function(event) {
-		event.preventDefault();
-		var link =  $(this).attr('href');
-		$('html, body').animate({ scrollTop: $(link).offset().top }, 800);
-	});
+  function showWorkFunc() {
+    for (var i = 0; i < 8; i++) {
+      $(work[showWork]).fadeIn(1000);
+      showWork++;
+    }
+    if ($(work).length <= showWork) {
+      $('.portfolio__show-more').hide(200);
+    }
+  };
 
-	$('.scroll-top').click(function(event) {
-		$('html, body').animate({ scrollTop: 0}, 800);
-	});
+  let work = $(".works").children();
+  work.hide();
+  $('.portfolio__show-more').on("click", showWorkFunc);
+  showWorkFunc();
 
-	$(document).scroll(	function () {
-		var top = $("html, body").scrollTop();
-		if (top > 200) {
-			$('.scroll-top').fadeIn(500);
-		} else {
-			$('.scroll-top').fadeOut(500);
-		}
-	});
+  // work block aspect ratio
 
+  var w = $('.work').css('width');
+  var h = parseInt(w) / 100 * 56.25;
+  $('.work').css('height', h + 'px');
+  $(window).resize(function() {
+    var w = $('.work').css('width');
+    var h = parseInt(w) / 100 * 56.25;
+    $('.work').css('height', h + 'px');
+  });
 
+  /*yes, yes, i know how do it by css style with wrapping div
 
-
-
-
-
-	$('.contacts_animation').focusin(function() {
-		var elemHeight = $(this).css('height');
-		var elemTop = parseInt(elemHeight) + 30;
-		var elem = $(this).next().css({'top': -elemTop + 'px', 'color': '#23c869'});
-		$(this).css({'border-color': '#23c869'});
-	});
-	$('.contacts_animation').focusout(function() {
-		var elemHeight = $(this).css('height');
-		var elemTop = parseInt(elemHeight) - 5;
-		v = $(this).val();
-		if (v) {
-			$(this).next().css({'color': '#404040' });
-			$(this).css({'color': '#27b262', 'font-family': 'mullerbold'});
-		} else {
-			$(this).next().css({'top': -elemTop + 'px',  'color': '#404040' });
-		}
-		$(this).css({'border-color': '#404040'});
-	});
-
-
-
-
-
-	$('.section').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated fadeIn',
-		offset: 100
-	});
-
-
-let showWork = 0;
-function showWorkFunc(){
-	for (var i = 0; i < 8; i++) {
-		$(elem[showWork]).fadeIn(1000);
-		showWork++;
+	.work-wrapper {
+    width: 100%;
+    height: 0px;
+    padding-bottom: 56.25%;
 	}
-	if($(elem).length <= showWork) {
-		$('.portfolio__show-more').hide(200);
-	}
-};
+	.work {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 100%;
+    width: 100%;
+	}*/
+
+  // want to do light and dark theme on site
+
+  // setTimeout (function(){
+  // 	$('body, .header, .portfolio, .about-me, .rubbb, .front, .contacts__soc span, .contacts, h1, h2, h3, p, .menu__home, li, .work').not('.scroll-top p').css({
+  // 		'color': '#fff',
+  // 		'background-color': '#141414',
+  // 		'border-color': '#141414',
+  // 	});
+  // 	$('.header__link, .portfolio__show-more, .contacts_animation, textarea ').css({
+  // 		'color': '#141414',
+  // 		'background-color': '#fff',
+  // 		'border-color': '#fff',
+  // 	});
+  //
+  // 	$(".header__link, .portfolio__show-more").hover(function() {
+  // 	  $(this).css({
+  // 			'color': '#fff',
+  // 			'background-color': '#141414',
+  // 			'border-color': '#fff',
+  // 		})
+  // 	});
+  // 	$(".header__link, .portfolio__show-more").mouseleave(function() {
+  // 	  $(this).css({
+  // 			'color': '#141414',
+  // 			'background-color': '#fff',
+  // 			'border-color': '#fff',
+  // 		})
+  // 	})
+  // }, 1000);
 
 
-let elem = $(".works").children();
-elem.hide();
-$('.portfolio__show-more').on("click", showWorkFunc);
-showWorkFunc();
+  // send form to mail
 
-	/*
-	setTimeout (function(){
-		$('body, .header, .portfolio, .about-me, .contacts, h1, h2, h3, p, span, .menu__home').removeClass('white').addClass('black');
-		$('.header__link, .portfolio__show-more ').removeClass('.white_button').addClass('.black_button');
-	}, 3000);
-	*/
+  $(function() {
+    $("#contacts__submit").click(function() {
+      let formData = new FormData();
+      formData.append("name", $("#contacts__name").val());
+      formData.append("email", $("#contacts__email").val());
+      formData.append("telegram", $("#contacts__telegram").val());
+      formData.append("message", $("#contacts__message").val());
 
+      $.ajax({
+        type: "POST",
+        url: "../formdata.php",
+        data: formData,
+        success: function() {
+          $("#contacts__name, #contacts__email, #contacts__telegram, #contacts__message").val('');
+          console.log('success');
+        }
+      });
+      return false;
+    });
+  });
 
-	$(function() {
-	    $("#contacts__submit").click(function() {
-					let formData = new FormData();
-					formData.append("name", $("#contacts__name").val());
-					formData.append("email", $("#contacts__email").val());
-					formData.append("telegram", $("#contacts__telegram").val());
-					formData.append("message", $("#contacts__message").val());
+  // Yandex.Metrika counter
 
-					$.ajax({
-						type: "POST",
-						url: "../formdata.php",
-						data: formData,
-						success: function(){
-							$("#contacts__name, #contacts__email, #contacts__telegram, #contacts__message").val('');
-							console.log('success');
-						}
-					});
-	        return false;
-	    });
-	});
+  (function(m, e, t, r, i, k, a) {
+    m[i] = m[i] || function() {
+      (m[i].a = m[i].a || []).push(arguments)
+    };
+    m[i].l = 1 * new Date();
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+  })
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-/* footer date */
+  ym(52417000, "init", {
+    id: 52417000,
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+  });
 
+  /* footer date */
 
-      let now = new Date();
-     	$(".footer_date").text(now.getFullYear());
+  let now = new Date();
+  $(".footer_date").text(now.getFullYear());
 
+  // about me old
+  let wasBorn = new Date(1995, 4, 7);
+  $(".about-me__old").text(Math.floor((now - wasBorn) / 31536000000));
 
 });
