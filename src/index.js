96551131cc24d922677f09b28dc5f4467e96bdda
gadzhi <gadzhi7.7.7@mail.ru>
2019-06-09 12:29:48 +0300

    some changes and add parallax effect to photo in block about me

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './screen.scss';
 import './media.scss';
 
 
-
 var $ = require("jquery");
 
 $(document).ready(function() {
-  // button scrollTop
 
+
+  // button scrollTop
   $(document).scroll(function() {
     let top = $("html, body").scrollTop();
     if (top > 200) {
@@ -19,9 +19,23 @@ $(document).ready(function() {
     }
   });
 
+  $('.scroll-top').click(scrollTo);
+
+  function scrollTo() {
+    let scroll = $("html, body").scrollTop()
+    if(scroll >= 200) {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 800);
+    } else {
+      $('html, body').animate({
+        scrollTop: document.body.scrollHeight
+      }, 800);
+    }
+  }
+
 
   // menu scroll
-
   $('.menu__link').click(function(event) {
     event.preventDefault();
     var link = $(this).attr('href');
@@ -31,151 +45,7 @@ $(document).ready(function() {
   });
 
 
-  // animation lable on contacts block
-
-  let elemHeight, elemTop, elem;
-
-  $('.contacts_animation').focusin(function() {
-    elemHeight = $(this).css('height');
-    elemTop = parseInt(elemHeight) + 30;
-    elem = $(this).next().css({
-      'top': -elemTop + 'px',
-      'color': '#23c869'
-    });
-    $(this).css({
-      'border-color': '#23c869'
-    });
-  });
-
-  $('.contacts_animation').focusout(function() {
-    elemHeight = $(this).css('height');
-    elemTop = parseInt(elemHeight) - 5;
-    let v = $(this).val();
-    if (v) {
-      $(this).next().css({
-        'color': '#404040'
-      });
-      $(this).css({
-        'color': '#27b262',
-        'font-family': 'mullerbold'
-      });
-    } else {
-      $(this).next().css({
-        'top': -elemTop + 'px',
-        'color': '#404040'
-      });
-    }
-    $(this).css({
-      'border-color': '#404040'
-    });
-  });
-
-
-
-
-  // let showWork = 0;
-  //
-  // function showWorkFunc() {
-  //   for (var i = 0; i < 8; i++) {
-  //     $(work[showWork]).fadeIn(1000);
-  //     showWork++;
-  //   }
-  //   if ($(work).length <= showWork) {
-  //     $('.portfolio__show-more').hide(200);
-  //   }
-  // };
-  //
-  // let work = $(".works").children();
-  // work.hide();
-  // $('.portfolio__show-more').on("click", showWorkFunc);
-  // showWorkFunc();
-
-
-
-
-
-  // work block aspect ratio
-
-  var w = $('.work').css('width');
-  var h = parseInt(w) / 100 * 56.25;
-  $('.work').css('height', h + 'px');
-  $(window).resize(function() {
-    var w = $('.work').css('width');
-    var h = parseInt(w) / 100 * 56.25;
-    $('.work').css('height', h + 'px');
-  });
-
-  /*yes, yes, i know how do it by css style with wrapping div
-
-	.work-wrapper {
-    width: 100%;
-    height: 0px;
-    padding-bottom: 56.25%;
-	}
-	.work {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    height: 100%;
-    width: 100%;
-	}*/
-
-  // want to do light and dark theme on site
-
-  // setTimeout (function(){
-  // 	$('body, .header, .portfolio, .about-me, .rubbb, .front, .contacts__soc span, .contacts, h1, h2, h3, p, .menu__home, li, .work').not('.scroll-top p').css({
-  // 		'color': '#fff',
-  // 		'background-color': '#141414',
-  // 		'border-color': '#141414',
-  // 	});
-  // 	$('.header__link, .portfolio__show-more, .contacts_animation, textarea ').css({
-  // 		'color': '#141414',
-  // 		'background-color': '#fff',
-  // 		'border-color': '#fff',
-  // 	});
-  //
-  // 	$(".header__link, .portfolio__show-more").hover(function() {
-  // 	  $(this).css({
-  // 			'color': '#fff',
-  // 			'background-color': '#141414',
-  // 			'border-color': '#fff',
-  // 		})
-  // 	});
-  // 	$(".header__link, .portfolio__show-more").mouseleave(function() {
-  // 	  $(this).css({
-  // 			'color': '#141414',
-  // 			'background-color': '#fff',
-  // 			'border-color': '#fff',
-  // 		})
-  // 	})
-  // }, 1000);
-
-
-  // send form to mail
-
-  $(function() {
-    $("#contacts__submit").click(function() {
-      let formData = new FormData();
-      formData.append("name", $("#contacts__name").val());
-      formData.append("email", $("#contacts__email").val());
-      formData.append("telegram", $("#contacts__telegram").val());
-      formData.append("message", $("#contacts__message").val());
-
-      $.ajax({
-        type: "POST",
-        url: "../formdata.php",
-        data: formData,
-        success: function() {
-          $("#contacts__name, #contacts__email, #contacts__telegram, #contacts__message").val('');
-          console.log('success');
-        }
-      });
-      return false;
-    });
-  });
-
   // Yandex.Metrika counter
-
   (function(m, e, t, r, i, k, a) {
     m[i] = m[i] || function() {
       (m[i].a = m[i].a || []).push(arguments)
@@ -194,7 +64,6 @@ $(document).ready(function() {
   });
 
   /* footer date */
-
   let now = new Date();
   $(".footer_date").text(now.getFullYear());
 
@@ -202,16 +71,24 @@ $(document).ready(function() {
   let wasBorn = new Date(1995, 4, 7);
   $(".about-me__old").text(Math.floor((now - wasBorn) / 31536000000));
 
-
-
   //loader
   setTimeout(() => $('.loader').hide(100), 200)
 
 });
 
 
+// parallax photo
+$(document).ready(function() {
+  let imgBlock = document.querySelector('.about-me')
+  let imgMy = document.querySelector('.about-me__photo')
+
+  window.onscroll = () => {
+    imgBlock.offsetTop + imgBlock.getBoundingClientRect().top <= 0 ? imgMy.style.position = 'absolute' : imgMy.style.position = 'fixed'
+  }
+})
 
 
+// day/night mode
 $(document).ready(function() {
   let dn = localStorage.getItem('dayNight');
   dn = JSON.parse(dn)
@@ -221,42 +98,31 @@ $(document).ready(function() {
   }
 })
 
+
 $('#toggle_checkbox').on('change', function() {
   localStorage.setItem('dayNight', JSON.stringify(this.checked));
   dayNight(this.checked)
 })
 
+let aboutMeImg = document.querySelector('.about-me__photo img')
+
 function dayNight (t = false) {
+  let now = new Date().toLocaleTimeString().substring(0, 2)
   if(t) {
     $('.day-text').removeClass('day-text').addClass('night-text')
     $('.day-bg').removeClass('day-bg').addClass('night-bg')
     $('.day-button').removeClass('day_button').addClass('night-button')
+    if(now > 23 || now < 6) aboutMeImg.setAttribute('src', 'img/about_me_night.jpg')
   } else {
     $('.night-text').removeClass('night-text').addClass('day-text')
     $('.night-bg').removeClass('night-bg').addClass('day-bg')
     $('.night-button').removeClass('night-button').addClass('day_button')
+    if(now > 23 || now < 6) aboutMeImg.setAttribute('src', 'img/about_me.jpg')
   }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// show works from JSON
 let allWorks = null
 
    let promise = fetch('works.json')
@@ -302,6 +168,21 @@ function addWorks(works) {
       $('.work').css('height', h + 'px');
     });
 
+    /*yes, yes, i know how do it by css style with wrapping div
+    .work-wrapper {
+      width: 100%;
+      height: 0px;
+      padding-bottom: 56.25%;
+    }
+    .work {
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      height: 100%;
+      width: 100%;
+    }*/
+
+
     showedWorkdIndex += 8
 
     works.length <= showedWorkdIndex ? $('.portfolio__show-more').hide(100) : null
@@ -310,37 +191,3 @@ function addWorks(works) {
 document.querySelector('.portfolio__show-more').addEventListener('click', () => {
   addWorks(allWorks)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // button scrollTop
-
-  $('.scroll-top').click(scrollTo);
-
-  function scrollTo() {
-    let scroll = $("html, body").scrollTop()
-    if(scroll >= 200) {
-        $('html, body').animate({
-          scrollTop: 0
-        }, 800);
-    } else {
-      $('html, body').animate({
-        scrollTop: document.body.scrollHeight
-      }, 800);
-    }
-  }
