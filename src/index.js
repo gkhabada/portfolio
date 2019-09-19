1d97ab9e59f2b9d73038ee4d515e0526121abc07
gadzhi <gadzhi7.7.7@mail.ru>
2019-09-19 08:13:07 +0300

    yandex metrika

--- src/index.js
+++ src/index.js
@@ -44,22 +44,15 @@ $(document).ready(function() {
   });
 
 
-  // Yandex.Metrika counter
-  (function(m, e, t, r, i, k, a) {
-    m[i] = m[i] || function() {
-      (m[i].a = m[i].a || []).push(arguments)
-    };
-    m[i].l = 1 * new Date();
-    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-  })
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-  ym(52417000, "init", {
-    id: 52417000,
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-    webvisor: true
+  ym(55392121, "init", {
+       clickmap:true,
+       trackLinks:true,
+       accurateTrackBounce:true,
+       webvisor:true
   });
 
   /* footer date */
