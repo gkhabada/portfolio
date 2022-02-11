7cf820b8bf1211c8f1ec06b09073729500b2b975
gkhabada <gadzhidadaev95@gmail.com>
2022-02-11 23:44:57 +0300

    refactor: works, cereer and other

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './scss/variables.scss';
 import './scss/reset.scss';
 import './scss/screen.scss';
 import './scss/media.scss';
@@ -6,30 +5,28 @@ import './scss/media.scss';
 document.addEventListener('DOMContentLoaded', function() {
 
   // button scrollTop
-  document.addEventListener('scroll', function() {
-    let top = document.querySelector('html').scrollTop;
-    if (top > 200) {
-      document.querySelector('.scroll-top p').style.transform = 'rotate(180deg)';
-    } else {
-      document.querySelector('.scroll-top p').style.transform = 'rotate(0deg)';
-    }
+  const scrollTopElem = document.querySelector('.scroll-top p');
+  const htmlElem = document.querySelector('html');
+
+  document.addEventListener('scroll', () => {
+    let top = htmlElem.scrollTop;
+    scrollTopElem.style.transform = `rotate(${top > 200 ? 180 : 0}deg)`;
   });
 
   document.querySelector('.scroll-top').addEventListener('click', function () {
-    let scroll = document.querySelector("html").scrollTop;
-    if(scroll >= 200) {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    } else {
-      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-    }
+    let scroll = htmlElem.scrollTop;
+    const scrollTo = scroll >= 200 ? 0 : document.body.scrollHeight;
+    window.scrollTo({ top: scrollTo, behavior: "smooth" });
   });
 
 
   // menu scroll
-  document.querySelector('.menu__link').addEventListener('click', function(event) {
-    event.preventDefault();
-    const link = event.target.attr('href');
-    document.querySelector(link).scrollIntoView();
+  document.querySelectorAll('.menu__link').forEach((item) => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      const link = event.target.getAttribute('href');
+      document.querySelector(link).scrollIntoView({ behavior: 'smooth' });
+    })
   });
 
   // footer date
@@ -39,10 +36,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // about me old
   let wasBorn = new Date(1995, 4, 7);
   document.querySelector(".about-me__old").innerText = Math.floor((now - wasBorn) / 31536000000);
-
-  //loader
-  setTimeout(() => document.querySelector('.loader').style.display = 'none', 100);
-
 
   // parallax photo
   const imgBlock = document.querySelector('.about-me')
@@ -54,88 +47,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   // day/night mode
-  const dn = JSON.parse(localStorage.getItem('dayNight'));
-  const dnCheckbox = document.querySelector('#toggle_checkbox');
-  const aboutMeImg = document.querySelector('.about-me__photo img');
+  // const dn = JSON.parse(localStorage.getItem('dayNight'));
+  // const dnCheckbox = document.querySelector('#toggle_checkbox');
+  // const aboutMeImg = document.querySelector('.about-me__photo img');
 
 
-  if(dn) {
-    dnCheckbox.checked = true;
-    dayNight(dn)
-  }
+  // if(dn) {
+  //   dnCheckbox.checked = true;
+  //   dayNight(dn)
+  // }
 
-  dnCheckbox.addEventListener('change', function() {
-    localStorage.setItem('dayNight', JSON.stringify(this.checked));
-    dayNight(this.checked)
-  });
-
-
-  function dayNight (time = false) {
-    let now = Number(new Date().toLocaleTimeString().substring(0, 2))
-    if(time) {
-      document.querySelector('.day-text').classList.add('night-text');
-      document.querySelector('.day-text').classList.remove('day-text');
-      document.querySelector('.day-bg').classList.add('night-bg');
-      document.querySelector('.day-bg').classList.remove('day-bg');
-      document.querySelector('.day-button').classList.add('night-button');
-      document.querySelector('.day-button').classList.remove('day_button');
-      if(now >= 23 || now <= 6) aboutMeImg.setAttribute('src', 'img/about_me_night.jpg');
-    } else {
-      document.querySelector('.night-text').classList.add('day-text');
-      document.querySelector('.night-text').classList.remove('night-text')
-      document.querySelector('.night-bg').classList.add('day-bg');
-      document.querySelector('.night-bg').classList.remove('night-bg');
-      document.querySelector('.night-button').classList.add('day_button');
-      document.querySelector('.night-button').classList.remove('night-button');
-      if(now >= 23 || now <= 6) aboutMeImg.setAttribute('src', 'img/about_me.jpg');
-    }
-  }
+  // dnCheckbox.addEventListener('change', function() {
+  //   localStorage.setItem('dayNight', JSON.stringify(this.checked));
+  //   dayNight(this.checked)
+  // });
 
 
-  // show works from JSON
-  let promise = fetch('./assets/works.json')
-    .then(function(response) {
-      if(response.status === 200) {
-        return response.json();
-      }
-      return null;
-    })
-    .then(function(json) {
-      if(json !== null) {
-        addWorks(json)
-      }
-    });
-
-  let worksDiv = document.querySelector('.works');
-  
-  function addWorks(works) {
-    if(works !== null && works.length) {
-      for(let i = 0; i < works.length; i++) {
-        if(works[i] !== undefined) {
-          let work = `
-          <div class="work">
-            <div class="work_hover">
-              <h3 class="work__title">${works[i].title}</h3>
-              <p class="work__text">${works[i].text}</p>
-              <a href="${works[i].link}" class="work__link" target="_blank" rel="noopener">Открыть</a>
-            </div>
-            <img src="./assets/${works[i].img}" alt="${works[i].title}" title="${works[i].title}">
-          </div>
-          `
-          worksDiv.insertAdjacentHTML('beforeend', work);
-        }
-      }
-      setWorkCardHeight();
-      document.addEventListener('resize', setWorkCardHeight);
-    }
-  }
-
-  function setWorkCardHeight() {
-    const w = document.querySelector('.work').clientWidth;
-    const h = parseInt(w) / 100 * 56.25;
-    const workCards = document.querySelectorAll('.work');
-    for (let card of workCards) {
-      card.style.height = `${h}px`;
-    }
-  }
+  // function dayNight (time = false) {
+  //   let now = Number(new Date().toLocaleTimeString().substring(0, 2))
+  //   if(time) {
+  //     document.querySelector('.day-text').classList.add('night-text');
+  //     document.querySelector('.day-text').classList.remove('day-text');
+  //     document.querySelector('.day-bg').classList.add('night-bg');
+  //     document.querySelector('.day-bg').classList.remove('day-bg');
+  //     document.querySelector('.day-button').classList.add('night-button');
+  //     document.querySelector('.day-button').classList.remove('day_button');
+  //     if(now >= 23 || now <= 6) aboutMeImg.setAttribute('src', 'img/about_me_night.jpg');
+  //   } else {
+  //     document.querySelector('.night-text').classList.add('day-text');
+  //     document.querySelector('.night-text').classList.remove('night-text')
+  //     document.querySelector('.night-bg').classList.add('day-bg');
+  //     document.querySelector('.night-bg').classList.remove('night-bg');
+  //     document.querySelector('.night-button').classList.add('day_button');
+  //     document.querySelector('.night-button').classList.remove('night-button');
+  //     if(now >= 23 || now <= 6) aboutMeImg.setAttribute('src', 'img/about_me.jpg');
+  //   }
+  // }
 });
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import pugPlugin from 'vite-plugin-pug';
+
+export default defineConfig({
+  base: '',
+  build: {
+    emptyOutDir: false,
+  },
+  plugins: [pugPlugin()],
+});
