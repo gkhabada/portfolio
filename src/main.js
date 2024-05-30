ea384ac8c947b54fe3078269195cc0440dade62e
gkhabada <gadzhidadaev95@gmail.com>
2024-05-30 16:40:00 +0300

    redesign

--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
 import './scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
-  // footer date
+  // footer year
   const now = new Date();
-  document.querySelector(".footer_date").innerText = now.getFullYear();
+  document.querySelector(".footer__text_year").innerText = now.getFullYear();
 
-  // about me old
   let wasBorn = new Date(1995, 4, 7);
-  document.querySelector(".about-me__old").innerText = Math.floor((now - wasBorn) / 31536000000);
+  document.querySelector(".about-me__old").innerText =  Math.floor((now - wasBorn) / 31536000000);
 
-  // parallax photo
-  const imgBlock = document.querySelector('.about-me')
-  const imgMy = document.querySelector('.about-me__photo')
-  document.addEventListener('scroll', () => {
-    const scrollToBottom = imgBlock.offsetTop + imgBlock.getBoundingClientRect().bottom;
-    imgMy.style.position = scrollToBottom <= 300 ? 'absolute' : 'fixed';
+  // active experience company
+  const blocks = Array.from(document.querySelectorAll('.experience__item'));
+  blocks.forEach((block) => {
+    block.addEventListener('mouseover', () => {
+      block.classList.add('active');
+    });
+
+    block.addEventListener('mouseout', () => {
+      blocks.forEach((b) => b.classList.remove('active'));
+    });
   });
+
+  blocks[blocks.length - 1].classList.add('active');
 });
