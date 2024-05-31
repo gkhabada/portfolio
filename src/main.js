76b0c37b4a0e1c30b588945dae5ad9a1f4083207
gkhabada <gadzhidadaev95@gmail.com>
2024-05-31 15:06:21 +0300

    redesign

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import './scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', function() {
+  // zero image animations
+  const zeroImage = document.querySelector('.zero__image');
+  zeroImage.addEventListener('mouseover', () => {
+    if (zeroImage.classList.contains('zero__image--circle')) {
+      zeroImage.classList.add('zero__image--square');
+      zeroImage.classList.remove('zero__image--circle');
+    } else {
+      zeroImage.classList.remove('zero__image--square');
+      zeroImage.classList.add('zero__image--circle');
+    }
+  });
+
   // footer year
   const now = new Date();
   document.querySelector(".footer__text_year").innerText = now.getFullYear();
@@ -12,13 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const blocks = Array.from(document.querySelectorAll('.experience__item'));
   blocks.forEach((block) => {
     block.addEventListener('mouseover', () => {
-      block.classList.add('active');
-    });
-
-    block.addEventListener('mouseout', () => {
-      blocks.forEach((b) => b.classList.remove('active'));
+      if (!block.classList.contains('active')) {
+        blocks.forEach((b) => b.classList.remove('active'));
+        block.classList.add('active');
+      }
     });
   });
-
   blocks[blocks.length - 1].classList.add('active');
+
 });
